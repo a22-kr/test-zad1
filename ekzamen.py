c57cf7200282efe8ed6f03afd6b88b3898378fd1
Aidar <a.abdyrahmanov99@gmail.com>
2018-03-06 19:52:42 +0600

    Add main file

---
+++ ekzamen.py
@@ -0,0 +1,9 @@
+for d in range(1,100):
+    if d%3==0:
+        print("LOLO")
+    elif d%5==0:
+        print("PEPE")
+    elif d%3==0 and d%5==0:
+        print("LOLOPEPE")
+    else:
+        print(d)
